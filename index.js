380ec51fdf7709968a3d2b27adcf550a8a29b785
Karnveer Grewal <karnveergrewal4@gmail.com>
2020-11-25 09:24:04 -0500

    finished project

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // your code here
+$(function () {
+  $("#number-chooser").on("submit", function (event) {
+    event.preventDefault();
+    $(".js-results").empty();
+    const countTo = $(event.currentTarget).find("#number-choice").val();
+    const results = [];
+    for (let i = 1; i <= countTo; i++) {
+      if (i % 15 === 0) {
+        results.push(
+          $("<div class='fizz-buzz-item fizzbuzz'><span>fizzbuzz</span></div>")
+        );
+      } else if (i % 5 === 0) {
+        results.push(
+          $("<div class='fizz-buzz-item buzz'><span>buzz</span></div>")
+        );
+      } else if (i % 3 === 0) {
+        results.push(
+          $("<div class='fizz-buzz-item fizz'><span>fizz</span></div>")
+        );
+      } else {
+        results.push($(`<div class='fizz-buzz-item'><span>${i}</span></div>`));
+      }
+    }
+    $(".js-results").append(results);
+  });
+});
